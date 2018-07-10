38a5786d519c053c99141ad63d4661f1f3c00d44
chawatvish <chawatvish.wo@gmail.com>
2018-07-10 17:30:33 +0700

    init project

---
+++ LocationWrapper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationWrapper
+//
+//  Created by Chawatvish Worrapoj on 10/7/2561 BE.
+//  Copyright © 2561 Chawatvish Worrapoj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+target 'LocationWrapper' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for LocationWrapper
+  pod 'SwiftLocation', '~> 3.2.3'
+end
